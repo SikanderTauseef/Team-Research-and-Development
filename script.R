b7de7429ebc60fc4518e660322b519c36edd7631
SikanderTauseef <Sikandertauseef752@gmail.com>
2025-01-03 12:41:51 +0500

    Perform anova test and generate anova_results.txt file

--- script.R
+++ script.R
@@ -2,7 +2,7 @@
 library(ggplot2)
 
 # Read in the sample data from a CSV file
-sample_data <- read.csv("/Users/obstechlaravel/Desktop/Team-Research-and-Development/Fat_Supply_Quantity_Data.csv")
+sample_data <- read.csv("/Users/sikandertauseef/Desktop/Projects/Team-Research-and-Development/Fat_Supply_Quantity_Data.csv")
 
 # Create a histogram visualization for Obesity Rates
 ggplot(sample_data, aes(x = Obesity)) + 
@@ -20,7 +20,7 @@ ggplot(sample_data, aes(x = Obesity)) +
   theme_minimal()
 
 # Save the plot as a PNG file
-ggsave("/Users/obstechlaravel/Desktop/Team-Research-and-Development/obesity_plot.png", plot = last_plot())
+ggsave("/Users/obstechlaravel/Desktop/sikandertauseef/Team-Research-and-Development/obesity_plot.png", plot = last_plot())
 
 # Open a connection to a text file to write analysis results
 fileConn <- file("analysis_results.txt")
@@ -36,3 +36,22 @@ writeLines(c(
 
 # Close the connection to the text file
 close(fileConn)
+
+# Open a connection to a text file to write ANOVA results in append mode
+anovaFileConn <- file("anova_results.txt", open = "a")
+
+predictor_variables <- c("Animal.Products", "Animal.fats", "Cereals.Excluding.Beer", "Eggs", "Fish.Seafood", "Fruits.Excluding.Wine", "Milk.Excluding.Butter", "Vegetal.Products", "Vegetable.Oils", "Vegetables")
+
+# Open a connection to a text file to write ANOVA results in append mode
+anovaFileConn <- file("anova_results.txt", open = "a")
+
+for (variable in predictor_variables) {
+  anova_result <- aov(Obesity ~ sample_data[, variable], data = sample_data)
+  writeLines(c(
+    paste("ANOVA table for", variable),
+    "",
+    capture.output(summary(anova_result))
+  ), anovaFileConn)
+}
+# Close the connection to the text file
+close(anovaFileConn)
